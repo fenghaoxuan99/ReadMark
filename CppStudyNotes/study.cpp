acd9695401017ceab4f34da7f93e95d7bfb63c3a
feng <1804831168@qq.com>
2025-03-26 14:51:57 +0800

    2025-03-26 14:52:00

--- CppStudyNotes/study.cpp
+++ CppStudyNotes/study.cpp
@@ -1,29 +1,29 @@
 #include <iostream>
 #include <sstream>
 
+template <class T>
+class test
+{
+public:
+    T &operator[](int i)
+    {
+        return value[i];
+    }
+
+private:
+    T value[10];
+};
+
 int main()
 {
-    std::stringstream s1("Hello, world"); // IO stream
-    s1.get();
-    if (s1.putback('Y')) // modifies the buffer
-        std::cout << s1.rdbuf() << '\n';
-    else
-        std::cout << "putback failed\n";
-
-    std::cout << "--\n";
-
-    std::istringstream s2("Hello, world"); // input-only stream
-    s2.get();
-    if (s2.putback('Y')) // cannot modify input-only buffer
-        std::cout << s2.rdbuf() << '\n';
-    else
-        std::cout << "putback failed\n";
-    s2.clear();
-
-    std::cout << "--\n";
-
-    if (s2.putback('H')) // non-modifying putback
-        std::cout << s2.rdbuf() << '\n';
-    else
-        std::cout << "putback failed\n";
+    test<test<int>> t;
+    for (int i = 0; i < 10; i++)
+    {
+        t[i][i] = i;
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << t[i][i] << std::endl;
+    }
+    return 0;
 }
