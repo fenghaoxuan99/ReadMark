604e1bb8f4df8ace76aaaf4eef588dc77990d0cb
feng <1804831168@qq.com>
2025-03-27 13:28:54 +0800

    2025-03-27 13:28:58

--- CppStudyNotes/study.cpp
+++ CppStudyNotes/study.cpp
@@ -1,29 +1,14 @@
+
+
 #include <iostream>
-#include <sstream>
 
-template <class T>
-class test
-{
-public:
-    T &operator[](int i)
-    {
-        return value[i];
-    }
 
-private:
-    T value[10];
-};
+
+
+
 
 int main()
 {
-    test<test<int>> t;
-    for (int i = 0; i < 10; i++)
-    {
-        t[i][i] = i;
-    }
-    for (int i = 0; i < 10; i++)
-    {
-        std::cout << t[i][i] << std::endl;
-    }
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
