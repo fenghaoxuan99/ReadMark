64a28fdb5e52106776257224670f52a354fff719
feng <1804831168@qq.com>
2025-04-29 14:58:22 +0800

    2025-04-29 14:58:25

--- CppStudyNotes/study.cpp
+++ CppStudyNotes/study.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
-#include <typeinfo>
 using namespace std;
 
-class A
+class Base
 {
 public:
-    virtual ~A();
+    virtual void fun()
+    {
+        cout << "base :: fun()" << endl;
+    }
 };
 
-using namespace std; 
+class Son : public Base
+{
+public:
+    virtual void fun()
+    {
+        cout << "son :: fun()" << endl;
+    }
+    void func()
+    {
+        cout << "son :: not virtual function" << endl;
+    }
+};
+
 int main()
 {
-    A *a = NULL;
-    try
-    {
-        cout << typeid(*a).name() << endl; // Error condition
-    }
-    catch (bad_typeid)
-    {
-        cout << "Object is NULL" << endl;
-    }
+    Son s;
+    Base &b = s; // 基类类型引用绑定已经存在的Son对象，引用必须初始化
+    s.fun();     // son::fun()
+    b.fun();     // son :: fun()
     return 0;
 }
